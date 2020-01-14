2558886622e8c619d696670341e7e65a23fcb450
Felix Kugler <relgukxilef@gmail.com>
2020-01-14 23:24:54 +0100

    move training code to Develop folder

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dense-depth-fkugler", 
+    version="1.0.0",
+    author="Felix Kugler",
+    author_email="e1526144@student.tuwien.ac.at",
+    description="Dense depth estimation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/relgukxilef/dense-depth-estimation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
