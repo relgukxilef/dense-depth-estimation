84007e1d18f2126eb9606274680b56f72f91249f
Felix Kugler <relgukxilef@gmail.com>
2020-01-17 21:45:47 +0100

    remove unused code

--- Develop/DenseDepth-master/loss.py
+++ Develop/DenseDepth-master/loss.py
@@ -21,15 +21,10 @@ def depth_variance(y_true, y_pred):
     return tf.reduce_sum(variance * mask) / tf.reduce_sum(mask)
 
 def error_variance(y_true, y_pred):
-    depth = y_true[:, :, :, 0]
     mask = y_true[:, :, :, 1]
     depth_pred = y_pred[:, :, :, 0]
     depth2_pred = y_pred[:, :, :, 1]
 
-    variance = tf.squared_difference(depth, depth_pred)
     variance_pred = tf.maximum(0.0, depth2_pred - depth_pred * depth_pred)
-    variance_variance = tf.squared_difference(
-        tf.sqrt(variance), tf.sqrt(variance_pred)
-    )
-
+    
     return tf.reduce_sum(variance_pred * mask) / tf.reduce_sum(mask)
